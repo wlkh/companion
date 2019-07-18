54a52e972bcae43deaf4f323896ce97cd8858253
Willian Galvani <williangalvani@gmail.com>
2019-07-18 23:27:29 +0000

    ping_enumerator.py: rewrite to support both ping devices

--- tools/ping_enumerator.py
+++ tools/ping_enumerator.py
@@ -3,76 +3,165 @@
 """Scan serial ports for ping devices
     Symlinks to detected devices are created under /dev/serial/ping/
     This script needs root permission to create the symlinks
-    Exits with the number of ping devices detected
 """
+from __future__ import print_function
 import subprocess
-from brping import Ping1D
+from brping import *
+import serial
+import time
 
-def main():
-    """Main function
-        Enumerate new ping devices
-    """
-    output = ""
-    # Remove any previously created links
-    try:
-        output = subprocess.check_output(["rm", "-rf", "/dev/serial/ping"])
-    except subprocess.CalledProcessError as exception:
-        print(exception)
+class PingEnumerator:
+    ser = None
 
-    # Look for connected serial devices
-    try:
-        output = subprocess.check_output("ls /dev/serial/by-id", shell=True)
-    except subprocess.CalledProcessError as exception:
-        print(exception)
-        exit(1)
+    def legacy_detect_ping1d(self):
+        """
+        Detects Ping1D devices without DEVICE_INFORMATION implemented
+        """
+        firmware_version = self.request_message(PING1D_FIRMWARE_VERSION)
+        if firmware_version is None  or not hasattr(firmware_version, "device_type"):
+            return None
+        description = "/dev/serial/ping/Ping1D-id-%s-t-%s-m-%s-v-%s.%s" % (
+            firmware_version.src_device_id,
+            firmware_version.device_type,
+            firmware_version.device_model,
+            firmware_version.firmware_version_major,
+            firmware_version.firmware_version_minor
+        )
+        return description
 
-    pings_found = 0
+    def request_message(self, requested_id):
+        print("Requesting ", requested_id)
+        timeout = time.time() + 1
+        p = PingParser()
+        device_info = None
+        while device_info is None and time.time() < timeout:
+            m = PingMessage(PING1D_GENERAL_REQUEST)
+            m.requested_id = requested_id
+            m.pack_msg_data()
+            self.ser.write(m.msg_data)
+            time.sleep(0.01)
+            while self.ser.in_waiting:
+                if p.parse_byte(self.ser.read()) == PingParser.NEW_MESSAGE:
+                    msg = p.rx_msg
+                    #print(device_info)
+                    if msg.message_id == requested_id:
+                        return msg
+        print("No valid response!")
+        return None
 
-    # Look at each serial device, probe for ping
-    for dev in output.split('\n'):
-        if not dev:
-            continue
 
-        byiddev = "/dev/serial/by-id/" + dev
+    def detect_device(self, dev):
+        """
+        Attempts to detect the Ping device attached to serial port 'dev'
+        Returns the new path with encoded name if detected, or None if the
+        device was not detected
+        """
 
-        print("Looking for Ping at %s" % byiddev)
-        new_ping = Ping1D(byiddev)
+        self.ser = serial.Serial("/dev/serial/by-id/" + dev, 115200)
+        device_info = self.request_message(PING1D_DEVICE_INFORMATION)
 
-        if not new_ping.initialize():
-            continue
+        if not device_info or not hasattr(device_info, "device_type"):
+            return self.legacy_detect_ping1d()
 
+        if device_info.device_type == 1:
+            description = "/dev/serial/ping/Ping1D-id-%s-r-%s-v-%s.%s.%s"
+        elif device_info.device_type == 2:
+            description = "/dev/serial/ping/Ping360-id-%s-r-%s-v-%s.%s.%s"
+            # Open device with 2M baud to setup Ping360
+            print("Setting baud to 2M...")
+            ser = serial.Serial("/dev/serial/by-id/" + dev, 2000000)
+            ser.send_break()
+            ser.write("UUUUUUU")
+            ser.close()
+            self.set_low_latency(dev)
+
+        else:
+            return None
+
+        return description % (
+            device_info.src_device_id,
+            device_info.device_revision,
+            device_info.firmware_version_major,
+            device_info.firmware_version_minor,
+            device_info.firmware_version_patch
+        )
+
+    def set_low_latency(self, dev):
+        """
+        Receives /dev/serial/by-id/...
+        maps to it to ttyUSB and sets the latency_timer for the device
+        """
+        target_device = subprocess.check_output(' '.join(["readlink", "-f", "/dev/serial/by-id/%s" % dev]), shell=True)
+        device_name = target_device.split("/")[-1].strip()
+
+        latency_file = "/sys/bus/usb-serial/devices/{0}/latency_timer".format(device_name)
+
+        with open(latency_file, 'w') as p:
+            p.write("1")
+            p.flush()
+
+    def make_symlink(self, origin, target):
+        """
+        follows target to real device an links origin to it
+        origin => target
+        Returns True if sucessful
+        """
         try:
-            device_id = new_ping.get_device_id()
-            firmware_version = new_ping.get_firmware_version()
-            if  device_id and firmware_version:
-                # ex Ping1D-id-43-t-1-m-1-v-3.19
-                description = "/dev/serial/ping/Ping1D-id-%s-t-%s-m-%s-v-%s.%s" % (
-                device_id["device_id"],
-                firmware_version["device_type"],
-                firmware_version["device_model"],
-                firmware_version["firmware_version_major"],
-                firmware_version["firmware_version_minor"])
+            # Follow link to actual device
+            target_device = subprocess.check_output(' '.join(["readlink", "-f", "/dev/serial/by-id/%s" % origin]), shell=True)
+            # Strip newline from output
+            target_device = target_device.decode().split('\n')[0]
 
-                print("Found Ping1D (ID: %d) at %s" % (device_id["device_id"], dev))
-
-                # Follow link to actual device
-                target_device = subprocess.check_output(' '.join(["readlink", "-f", byiddev]), shell=True)
-                # Strip newline from output
-                target_device = target_device.split('\n')[0]
-
-                # Create another link to it
-                print("Creating symbolic link to", target_device)
-                subprocess.check_output(' '.join(["mkdir", "-p", "/dev/serial/ping"]), shell=True)
-                output = subprocess.check_output("ln -fs %s %s" % (
-                    target_device,
-                    description), shell=True)
-                pings_found += 1
-
+            # Create another link to it
+            subprocess.check_output(' '.join(["mkdir", "-p", "/dev/serial/ping"]), shell=True)
+            subprocess.check_output("ln -fs %s %s" % (
+                target_device,
+                target), shell=True)
+            print(origin, " linked to ", target)
+            return True
         except subprocess.CalledProcessError as exception:
             print(exception)
-            continue
+            return False
 
-    exit(pings_found)
+
+    def erase_old_symlinks(self):
+        """
+        Erases all symlinks at "/dev/serial/ping/"
+        """
+        try:
+            subprocess.check_output(["rm", "-rf", "/dev/serial/ping"])
+        except subprocess.CalledProcessError as exception:
+            print(exception)
+
+
+    def list_serial_devices(self):
+        """
+        Lists serial devices at "/dev/serial/by-id/"
+        """
+        # Look for connected serial devices
+        try:
+            output = subprocess.check_output("ls /dev/serial/by-id", shell=True)
+            return output.decode().strip().split("\n")
+        except subprocess.CalledProcessError as exception:
+            print(exception)
+            return []
+
+
+    def __init__(self):
+        """Main function
+            Enumerate new ping devices
+        """
+        self.erase_old_symlinks()
+
+        # Look at each serial device, probe for ping
+        for dev in self.list_serial_devices():
+            link = self.detect_device(dev)
+            if link:
+                self.make_symlink(dev, link)
+            else:
+                print("Unable to identify device at ", dev)
+            self.ser.close()
+
 
 if __name__ == '__main__':
-    main()
+    PingEnumerator()
