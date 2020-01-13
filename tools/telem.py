a40c32f362eb562144c14c53086839db1bac020a
Patrick José Pereira <patrickelectric@gmail.com>
2020-01-13 17:59:37 -0300

    telem: Remove trailing spaces
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- tools/telem.py
+++ tools/telem.py
@@ -2,7 +2,7 @@
 
 import os
 from time import sleep
-home = os.environ['HOME'] 
+home = os.environ['HOME']
 
 while True:
   while ((os.system("ls /dev/autopilot 2>/dev/null") != 0) or (os.path.isfile(home+"/companion/scripts/start_mavproxy_telem_splitter.sh")==0)):
