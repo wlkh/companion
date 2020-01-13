a1c101e84c5bb17fdd545294ee58c1afed112e5f
Patrick José Pereira <patrickelectric@gmail.com>
2020-01-13 17:37:44 -0300

    endpoint: Remove trailing spaces
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- tools/endpoint.py
+++ tools/endpoint.py
@@ -9,17 +9,17 @@ debug = False
 endpoints = []
 
 class Endpoint(object):
-	
+
 	def __init__(self, id, type, connectionIds):
-		
+
 		# unique
 		self.id = id
 		self.type = type
 		self.connectionIds = connectionIds
 		# target destinations for inbound traffic
 		self.connections = []
-		
-		
+
+
 	def connect(self, target):
 		if target.id == self.id:
 			print("loopback not allowed: %s") % self.id
@@ -29,35 +29,35 @@ class Endpoint(object):
 			return
 		self.connections.append(target)
 		self.connectionIds.append(target.id)
-		
-		
+
+
 	def disconnect(self, target_id):
 		try:
 			self.connectionIds.remove(target_id)
 		except:
 			print("Error disconnecting %s") % target.id
 			return
-		
+
 		for endpoint in self.connections:
 			if endpoint.id == target_id:
 				self.connections.remove(endpoint)
 
 
 class SerialEndpoint(Endpoint):
-	
+
 	def __init__(self, port, baudrate, id, connections):
 		Endpoint.__init__(self, id, 'serial', connections)
 		self.port = port
 		self.baudrate = baudrate
 		self.active = False
-		
+
 		# not a socket! just a port
 		self.socket = serial.Serial()
 		self.socket.port = port
 		self.socket.baudrate = baudrate
 		self.socket.timeout = 0
-		
-		
+
+
 	def read(self):
 		try:
 			if not self.socket.is_open:
@@ -70,41 +70,41 @@ class SerialEndpoint(Endpoint):
 			self.active = False
 			#print("Error reading serial endpoint: %s") % e
 			return
-		
+
 		if len(data) > 0:
 			if debug:
 				#this works fine on rpi, but not desktop (ubuntu 16) for some reason
 				#print('%s read %s') % (self.id, data[:25].decode('utf-8'))
 				print('%s read') % self.id
-				
+
 			# write data out on all outbound connections
 			for endpoint in self.connections:
 				endpoint.write(data)
-	
-	
+
+
 	def write(self, data):
 		try:
 			if self.socket.is_open:
 				self.socket.write(data)
 				if debug:
 					print('%s write %s') % (self.id, data[:25])
-				
+
 		# serial.SerialException
 		except Exception as e:
 			print("Error writing: %s") % e
 			return
-		
-		
+
+
 	def to_json(self):
 		return {"id": self.id,
 				"type": self.type,
 				"port": self.port,
 				"baudrate": self.baudrate,
 				"connections": self.connectionIds};
-				
-		
+
+
 class UDPEndpoint(Endpoint):
-	
+
 	def __init__(self, ip, port, id, connections):
 		Endpoint.__init__(self, id, 'udp', connections)
 		self.ip = ip
@@ -115,14 +115,14 @@ class UDPEndpoint(Endpoint):
 		if (self.ip == '0.0.0.0'):
 			print('binding')
 			self.socket.bind((ip, int(port)))
-		
+
 	def read(self):
 		try:
 			data, address = self.socket.recvfrom(1024)
 			self.destination = address
 		except:
 			return
-		
+
 		if len(data) > 0:
 			if debug:
 				#print('%s read %s on %s') % (self.id, data[:25], address)
@@ -130,14 +130,14 @@ class UDPEndpoint(Endpoint):
 
 			for endpoint in self.connections:
 				endpoint.write(data)
-				
+
 	def write(self, data):
 		try:
 			if (self.ip == '0.0.0.0'):
 				self.socket.sendto(data, self.destination)
 			else:
 				self.socket.sendto(data, (self.ip, int(self.port)))
-			
+
 			if debug:
 				#print('%s write %s') % (self.id, data[:25])
 				print("%s write") % self.id
@@ -165,7 +165,7 @@ def add(new_endpoint):
 			existing_endpoint.connections.append(new_endpoint)
 		if existing_endpoint.id in new_endpoint.connectionIds:
 			new_endpoint.connections.append(existing_endpoint)
-			
+
 	endpoints.append(new_endpoint)
 
 
@@ -175,23 +175,23 @@ def remove(endpoint_id):
 		if endpoint.id == endpoint_id:
 			remove = endpoint
 			break
-		
+
 	if remove is None:
 		print("Error removing endpoint %s, id doesn't exist") % endpoint_id
 		return
-	
+
 	print("remove: %s") % remove
 	try:
 		remove.socket.close()
 		endpoints.remove(remove)
 		print("removed endpoint %s") % remove.id
-		
-		
+
+
 		for endpoint in endpoints:
 			endpoint.connections.remove(remove)
 			endpoint.connections.connectionIds.remove(remove.id)
-				
-				
+
+
 	except Exception as e:
 		#print("Error removing: %s") % e
 		pass
@@ -211,14 +211,14 @@ def from_json(endpoint_json):
 							endpoint_json['baudrate'],
 							endpoint_json['id'],
 							endpoint_json['connections'])
-		
+
 	elif endpoint_json['type'] == 'udp':
 		new_endpoint = UDPEndpoint(
 							endpoint_json['ip'],
 							endpoint_json['port'],
 							endpoint_json['id'],
 							endpoint_json['connections'])
-	
+
 	return new_endpoint
 
 
@@ -230,13 +230,13 @@ def connect(source_id, target_id):
 			source = endpoint
 		if endpoint.id == target_id:
 			target = endpoint
-			
+
 	if source is None:
 		print("Error: source %s is not present") % source_id
-		
+
 	if target is None:
 		print("Error: target %s is not present") % target_id
-		
+
 	source.connect(target)
 
 
@@ -246,12 +246,12 @@ def disconnect(source_id, target_id):
 	for endpoint in endpoints:
 		if endpoint.id == source_id:
 			source = endpoint
-			
+
 	if source is None:
 		print("Error: source %s is not present") % source_id
-		
+
 	#it's ok if target does not exist, it may still be a desired endpoint
-		
+
 	source.disconnect(target_id)
 
 
@@ -273,7 +273,7 @@ def load(filename):
 	except Exception as e:
 		print("Error loading from file %s: %s") % (filename, e)
 		return
-	
+
 	for endpoint in configuration['endpoints']:
 		try:
 			if endpoint['type'] == 'serial':
@@ -282,15 +282,15 @@ def load(filename):
 									endpoint['baudrate'],
 									endpoint['id'],
 									endpoint['connections'])
-				
+
 			elif endpoint['type'] == 'udp':
 				new_endpoint = UDPEndpoint(
 									endpoint['ip'],
 									endpoint['port'],
 									endpoint['id'],
 									endpoint['connections'])
-			
+
 			add(new_endpoint)
-		
+
 		except Exception as e:
 			print(e)
