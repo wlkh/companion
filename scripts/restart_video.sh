70ec32318780e4a27ad9a6d761bb549e20120c4b
Patrick José Pereira <patrickelectric@gmail.com>
2020-01-17 09:50:51 -0300

    restart_video: Check number of parameters before cleaning vidformat file
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- scripts/restart_video.sh
+++ scripts/restart_video.sh
@@ -7,10 +7,14 @@ fi
 # this is followed up in start_video.sh
 cp /home/pi/vidformat.param /home/pi/vidformat.param.bak
 
-echo $1 > /home/pi/vidformat.param
-echo $2 >> /home/pi/vidformat.param
-echo $3 >> /home/pi/vidformat.param
-echo $4 >> /home/pi/vidformat.param
+if [ "$#" == 4 ]; then
+        echo $1 > /home/pi/vidformat.param
+        echo $2 >> /home/pi/vidformat.param
+        echo $3 >> /home/pi/vidformat.param
+        echo $4 >> /home/pi/vidformat.param
+else
+        echo "Invalid number of parameters [$#]: $@"
+fi
 
 sudo -H -u pi screen -dm -S video /home/pi/companion/tools/streamer.py
 
