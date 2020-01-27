b445ad54571c455585d71df079542aeeb1ca279a
Patrick José Pereira <patrickelectric@gmail.com>
2020-01-27 18:11:29 -0300

    Revert "scripts/start_audio: broadcast output on the subnet"
    This reverts commit 18f4c9924f86ef1eded00bca86191593b94aeb8e.

--- scripts/start_audio.sh
+++ scripts/start_audio.sh
@@ -1,6 +1,6 @@
 export LD_LIBRARY_PATH=/usr/local/lib/
 cd $HOME/
-gst-launch-1.0 -v -e alsasrc device=hw:1,0 ! audioconvert ! rtpL16pay ! udpsink host=192.168.2.255 port=5601
+gst-launch-1.0 -v -e alsasrc device=hw:1,0 ! audioconvert ! rtpL16pay ! udpsink host=192.168.2.1 port=5601
 
 
 #gst-launch-1.0 -v -e alsasrc device=hw:1,0 ! audioconvert ! tcpserversink host=192.168.2.2 port=5700
