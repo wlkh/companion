da5234ac1bb50f3b35bef2bc425c25b0138f0bed
Patrick José Pereira <patrickelectric@gmail.com>
2020-06-26 13:05:44 -0300

    scripts: start_webui: Remove ssh-agent
    This requires password input from the user
    
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- scripts/start_webui.sh
+++ scripts/start_webui.sh
@@ -8,9 +8,5 @@ tail -n 10000 $HOME/.webui.log > /tmp/.webui.log
 cp /tmp/.webui.log $HOME/.webui.log
 rm -f /tmp/.webui.log
 
-# start ssh-agent for git/ssh authentication
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_rsa
-
 # start webserver
 node index.js 2>&1 | tee -a $HOME/.webui.log
